f2f42291eda17e365f4f9659cd1b54779b8a54c3
Revanth jallipalli <81552258+123revanth@users.noreply.github.com>
2022-03-03 09:48:41 +0530

    Create FactorFInder.java

---
+++ FactorFInder.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class java {
+    public static void main(String[] args) {
+        
+     Scanner sc = new Scanner(System.in);
+     System.out.print("Enter the number");
+     int number = sc.nextInt();
+     
+     System.out.print("Factors of " + number + "are: ");
+
+     for(int i = 1; i <= number; i++){
+       if(number%i == 0){
+         System.out.print(i + " ");
+       }
+     }
+
+    }
+  }
