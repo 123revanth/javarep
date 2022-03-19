0c18d68b48155457963d484d7ecfaf98d6cfac75
Revanth jallipalli <81552258+123revanth@users.noreply.github.com>
2022-03-19 09:07:29 +0530

    Create CONTINUED_NUMBER_PRINT.java

---
+++ CONTINUED_NUMBER_PRINT.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class java {
+    public static void main(String[] args) {
+        
+     Scanner sc = new Scanner(System.in);
+     System.out.println("Enter the first and last number:");
+     int numberone = sc.nextInt();
+     int numbertwo = sc.nextInt();
+
+     System.out.println("OUTPUT");
+
+     for(int i = numberone; i <= numbertwo; i++){
+       switch(i){
+         case (15):
+         continue;
+         case (16):
+         continue;
+         case (17):
+         continue;
+         default:
+         System.out.println(i);
+
+       }
+     }
+
+     }
+     }
+    
+  
